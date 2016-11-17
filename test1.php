b05c76bef44637c24e185f1ffe371a9c600b05bb
chryslemm <christian_leuyam@hotmail.com>
2016-11-17 10:24:27 +0100

    test001

---
+++ test1.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Test - Page 1</title>
+	</head>
+	<body>
+		<p>Hello, check time <a href="test2.php">here</a><p>
+	</body>
+</html>
---
+++ test2.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Test - Page 2</title>
+	</head>
+	<body>
+		<p>It's <?php echo date('H:i') . ' GMT'; ?></p>
+		<p><a href="test1.php">Back</a></p>
+	</body>
+</html>
